73bdf2d37918daec57f3a7cbb0fc464b926576cd
Disciple of Jesus <Disciple-of-jesus@github.com>
2024-10-30 14:45:57 +0100

    30-10-2024: Simple implementation with IndexedDB.

--- cypress/rememberWord.spec.js
+++ cypress/rememberWord.spec.js
@@ -1,22 +1,17 @@
 describe('Remember Word Functionality', () => {
     beforeEach(() => {
-        // Visit your application before each test
         cy.visit('../index.html');
-        
-        // Clear local storage before each test to have a clean state
-        cy.clearLocalStorage();
     });
 
     it('should add a word to the list of remembered words', () => {
-        const word = 'Test Word';
+        const word = 'This is the word of God to me today!';
 
-        // Type the word into the input field
         cy.get('#wordToRemember').type(word);
 
-        // Click the remember button
         cy.contains('button', 'Onthouden').click();
 
-        // Check if the word appears in the list
         cy.get('#listOfGodlyWords').contains('li', word).should('exist');
+
+        cy.get('#wordToRemember').should('be.empty');
     });
 });
