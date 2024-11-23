1454c667f50d76c8497c90b8310c1a1483ca7093
Disciple of Jesus <Disciple-of-jesus@github.com>
2024-11-23 13:38:12 +0100

    Use a form label

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,13 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Ebenezer</title>
-    
+
     @laravelPWA
 </head>
 <body>
 <form action="/add" method="POST">
     @csrf
 
+    <label>Naam</label>
     <input type="text" name="nameOfStone"/>
     <input type="text" name="wayOfShowing"/>
     <textarea name="contextToWord"></textarea>
