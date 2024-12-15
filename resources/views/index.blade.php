08bca22b45de8f2d0dfff7bf04b8460908bb56b3
Disciple of Jesus <Disciple-of-jesus@github.com>
2024-12-15 15:19:08 +0100

    Removed Laravel PWA

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,8 +6,6 @@
 
     <title>Ebenezer</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.2/css/bulma.min.css">
-
-    @laravelPWA
 </head>
 <body>
 <div class="container is-fluid">
