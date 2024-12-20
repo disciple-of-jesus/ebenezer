522bb0af016b0b44ebd8ddaea61c0780d5ec61ef
Disciple of Jesus <disciple-of-jesus@github.com>
2024-12-20 17:59:48 +0100

    Fix service worker path

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 let mix = require('laravel-mix');
 
 mix.js('resources/scripts/echo.js', 'public/dist')
-    .copy('resources/scripts/serviceworker.js', 'serviceworker.js')
+    .copy('resources/scripts/serviceworker.js', 'public/serviceworker.js')
     .sass('resources/styles/app.scss', 'public/dist');
