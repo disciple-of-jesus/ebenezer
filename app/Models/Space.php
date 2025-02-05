f305bf6c3cbf541d966539ca991551322db74ea1
Disciple of Jesus <disciple-of-jesus@github.com>
2025-02-05 16:57:57 +0100

    Removed code that is not needed

--- app/Models/Space.php
+++ app/Models/Space.php
@@ -2,17 +2,12 @@
 
 namespace App\Models;
 
-use Database\Factories\SpaceFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Space extends Model
 {
-    /** @use HasFactory<SpaceFactory> */
-    use HasFactory;
-
     use HasUuids;
 
     public $timestamps = false;
--- app/Models/StoneOfRemembrance.php
+++ app/Models/StoneOfRemembrance.php
@@ -2,16 +2,11 @@
 
 namespace App\Models;
 
-use Database\Factories\StoneOfRemembranceFactory;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class StoneOfRemembrance extends Model
 {
-    /** @use HasFactory<StoneOfRemembranceFactory> */
-    use HasFactory;
-
     public $timestamps = false;
 
     protected $table = 'stones_of_remembrance';
